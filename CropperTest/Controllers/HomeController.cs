245501fbf75e0dc9a92e48bf62acee85ad9f09ef
codesack <paras.sitoula@gmail.com>
2016-02-04 15:52:31 +1100

    Added Readme

---
+++ CropperTest/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using CropperTest.Helper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+
+namespace CropperTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+      
+
+        [HttpPost]
+        public virtual ActionResult CropImage(
+            string imagePath,
+            int? cropPointX,
+            int? cropPointY,
+            int? imageCropWidth,
+            int? imageCropHeight)
+        {
+            if (string.IsNullOrEmpty(imagePath)
+                || !cropPointX.HasValue
+                || !cropPointY.HasValue
+                || !imageCropWidth.HasValue
+                || !imageCropHeight.HasValue)
+            {
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+
+            byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
+            byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
+                        
+            string fileName = Path.GetFileName(imagePath);
+
+            try
+            {
+                FileHelper.SaveFile(croppedImage, Server.MapPath(imagePath));
+            }
+            catch (Exception ex)
+            {
+                //Log an error     
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
+
+            string photoPath = string.Concat("/",fileName);
+            return Json(new { photoPath = imagePath }, JsonRequestBehavior.AllowGet);
+        }
+
+      
+
+    }
+}
---
+++ CropperTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CropperTest.Startup))]
+namespace CropperTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
